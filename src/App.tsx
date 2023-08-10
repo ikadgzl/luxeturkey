7851b99cd74a645e5fd8e09ae278873b8c0897da
ikadgzl <ikadgzl@gmail.com>
2023-08-10 17:41:04 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,32 @@
-import BackgroundImage from '/assets/bg.png';
+import BackgroundImage from "/assets/bg.png";
 
 function App() {
+  const width = window.innerWidth;
+
   return (
     <div
-      className='app'
+      className="app"
       style={{
-        width: '100vw',
-        height: '100svh',
+        width: "100vw",
+        height: "100svh",
         backgroundImage: `url(${BackgroundImage})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        fontFamily: 'Misspiece',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        fontFamily: "Misspiece",
       }}
     >
-      <h1
-        className='header'
+      <img
         style={{
-          marginTop: 'auto',
-          fontWeight: 400,
-          color: 'rgba(135, 135, 135, 1)',
-          fontSize: 96,
+          width: `${width < 768 ? "100%" : ""}`,
         }}
-      >
-        Luxe Turkiye
-      </h1>
-      <h2
-        style={{
-          fontWeight: 400,
-          color: 'white',
-          fontSize: 44,
-        }}
-      >
-        We're currently working to enhance your online experience.
-      </h2>
-      <h3
-        style={{
-          marginTop: 48,
-          fontWeight: 400,
-          color: '#CC0000',
-          fontSize: 36,
-        }}
-      >
-        Coming Soon with New & Exciting Changes
-      </h3>
-      <p
-        style={{
-          marginTop: 'auto',
-          marginBottom: 32,
-          fontSize: 36,
-          color: 'white',
-        }}
-      >
-        Info@LuxeTurkiye.com
-      </p>
+        src="/assets/aa.png"
+      />
     </div>
   );
 }
